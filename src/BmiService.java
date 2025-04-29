d2fbd8194c56a633d5fce36fad1312f39ffed7c3
bugaga1993 <b-ios@mail.ru>
2025-04-29 22:12:26 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,12 +1,7 @@
-public class Main {
+public class BmiService {
 
-    public static void main(String[] args) {
-        BmiService service = new BmiService();
-
-        double height = 1.87; //Рост
-        int weight = 98; //Вес
-
-        double index = (int) (service.calculate(height, weight));
-        System.out.println("Масса тела = " + Math.round(index));
+    public double calculate(double height, int weight) {
+        double bmi = weight / Math.pow(height, 2);
+        return bmi;
     }
 }
