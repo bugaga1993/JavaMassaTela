f731793b49ae59a0f8579fe998a58a6f7d16ca67
bugaga1993 <b-ios@mail.ru>
2025-12-18 23:04:53 +0300

    BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
     public int calculate(double height, double weight) {
-        int index = (int) (weight / (height * height)) // (int) (weight / Math.pow(height, 2));
+        int index = (int) (weight / (height * height)) //(weight / Math.pow(height, 2));
         return index;
     }
 }
